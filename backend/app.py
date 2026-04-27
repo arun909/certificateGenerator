ce27f2df598cd7e73a2dc12c8839894dcf053640
arun909 <arunbabu7591@gmail.com>
2026-04-27 13:28:46 +0530

    Setup docker files

--- backend/app.py
+++ backend/app.py
@@ -1205,4 +1205,5 @@ def download_certificate(cert_id):
 
 if __name__ == "__main__":
     enforce_user_role_policy()
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
